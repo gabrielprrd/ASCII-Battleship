f525ec2984fc0f0635a55bd5510c541193f050ba
Gabriel Afonso <gabriel.afonso.contato@hotmail.com>
2021-02-28 21:50:36 -0100

    add dynamic PORT based on user input and encapsulate ascii display on a method

--- src/org/academiadecodigo/cachealots/battleship/BattleshipServer.java
+++ src/org/academiadecodigo/cachealots/battleship/BattleshipServer.java
@@ -5,6 +5,7 @@ import java.net.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -29,7 +30,11 @@ public class BattleshipServer {
 
     public BattleshipServer() throws IOException {
 
-        serverSocket = new ServerSocket(8080);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input port number: ");
+        int port = scanner.nextInt();
+
+        serverSocket = new ServerSocket(port);
         threadPool = Executors.newCachedThreadPool();
         players = Collections.synchronizedList(new ArrayList<>());
 
@@ -42,20 +47,7 @@ public class BattleshipServer {
         // we need to broadcast this message
         System.out.println("Starting BattoruShippo server...\n\n");
 
-        System.out.println("\n" +
-                "                                     |__\n" +
-                "                                     |\\/\n" +
-                "                                     ---\n" +
-                "                                     / | [\n" +
-                "                              !      | |||\n" +
-                "                            _/|     _/|-++'\n" +
-                "                        +  +--|    |--|--|_ |-\n" +
-                "                     { /|__|  |/\\__|  |--- |||__/\n" +
-                "                    +---------------___[}-_===_.'____                 /\\ \n" +
-                "                ____`-' ||___-{]_| _[}-  |     |_[___\\==--            \\/   _\n" +
-                " __..._____--==/___]_|__|_____________________________[___\\==--____,------' .7\n" +
-                "|                                                           USS Cachealots /\n" +
-                " \\_________________________________________________________________________|\n");
+        System.out.println(drawWelcomeBoat());
 
         System.out.println("Server running on port: " + serverSocket.getLocalPort());
 
@@ -92,6 +84,33 @@ public class BattleshipServer {
 
     public String getAddress() throws UnknownHostException {
         return InetAddress.getLocalHost().getHostAddress() + ":" + serverSocket.getLocalPort();
+    }
+
+    public String drawWelcomeBoat() {
+        return ("\n" +
+                "                                     |__\n" +
+                "                                     |\\/\n" +
+                "                                     ---\n" +
+                "                                     / | [\n" +
+                "                              !      | |||\n" +
+                "                            _/|     _/|-++'\n" +
+                "                        +  +--|    |--|--|_ |-\n" +
+                "                     { /|__|  |/\\__|  |--- |||__/\n" +
+                "                    +---------------___[}-_===_.'____                 /\\ \n" +
+                "                ____`-' ||___-{]_| _[}-  |     |_[___\\==--            \\/   _\n" +
+                " __..._____--==/___]_|__|_____________________________[___\\==--____,------' .7\n" +
+                "|                                                           USS Cachealots /\n" +
+                " \\_________________________________________________________________________|\n" +
+
+        "" +
+                " ______                                     ______ _     _                   \n" +
+                "(____  \\         _     _                   / _____) |   (_)                  \n" +
+                " ____)  )_____ _| |_ _| |_ ___   ____ _   ( (____ | |__  _ ____  ____   ___  \n" +
+                "|  __  ((____ (_   _|_   _) _ \\ / ___) | | \\____ \\|  _ \\| |  _ \\|  _ \\ / _ \\ \n" +
+                "| |__)  ) ___ | | |_  | || |_| | |   | |_| |____) ) | | | | |_| | |_| | |_| |\n" +
+                "|______/\\_____|  \\__)  \\__)___/|_|   |____(______/|_| |_|_|  __/|  __/ \\___/ \n" +
+                "                                                          |_|   |_|          " +
+                "\n\n");
     }
 
 }
